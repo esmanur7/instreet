e9a5ff7fc9a5843431985e50986a689e2047c189
esmanur7 <aesmanur15@gmail.com>
2024-12-02 13:14:47 +0300

    Updated app.vue and added ImageSlider component

--- app.vue
+++ app.vue
@@ -2,11 +2,13 @@
 <template>
   <div>
     <SearchBar /> <!-- Arama çubuğu bileşeni -->
+    <ImageSlider />
     <!-- Diğer bileşenleri buraya ekleyebilirsin -->
   </div>
 </template>
 
 <script setup>
 import SearchBar from './components/SearchBar.vue'; // Bileşeni içe aktarma
+import ImageSlider from '~/components/ImageSlider.vue';
 </script>
 */
