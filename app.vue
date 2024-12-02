5f90eb703fc69d6768738eaf6d6a02db0e34d8bf
esmanur7 <aesmanur15@gmail.com>
2024-12-02 14:21:30 +0300

    Update App.vue and ImageSlider.vue

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
   <div>
     <SearchBar /> <!-- Arama çubuğu bileşeni -->
     <ImageSlider />
+    <CategoryCircles />
     <!-- Diğer bileşenleri buraya ekleyebilirsin -->
   </div>
 </template>
@@ -10,5 +11,6 @@
 <script setup>
 import SearchBar from './components/SearchBar.vue'; // Bileşeni içe aktarma
 import ImageSlider from '~/components/ImageSlider.vue';
+import CategoryCircles from '~/components/CategoryCircles.vue';
 </script>
 */
