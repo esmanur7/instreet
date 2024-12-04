e64651c4b84b3a06f4c53657f65ca79970f1f430
esmanur7 <111688118+esmanur7@users.noreply.github.com>
2024-12-04 11:43:56 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: [
-    'bootstrap/dist/css/bootstrap.css', // Bootstrap CSS dosyasını dahil et
+    'bootstrap/dist/css/bootstrap.css', 
+    '@fortawesome/fontawesome-free/css/all.min.css'// Bootstrap CSS dosyasını dahil et
   ],
   vite: {
     optimizeDeps: {
