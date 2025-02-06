dd31f7d5d381fcb7cf36c503a125dd3ea124c464
esmanur7 <111688118+esmanur7@users.noreply.github.com>
2025-02-07 00:29:10 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,6 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: [
-    'bootstrap/dist/css/bootstrap.css', 
-    '@fortawesome/fontawesome-free/css/all.min.css',// Bootstrap CSS dosyasını dahil et
-    'bootstrap-icons/font/bootstrap-icons.css' // Bootstrap Icons'ı ekle
-  ],
-  plugins: [
-    '~/plugins/firebase'
-  ],
-  vite: {
-    optimizeDeps: {
-      include: ['bootstrap'] // Bootstrap gibi kütüphaneleri optimize et
-    }
-  },
   modules: ['@pinia/nuxt'],
-
-});
+})
