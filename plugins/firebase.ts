5135c637cceeb9579d876491ede7df2f1b0c75b4
esmanur7 <111688118+esmanur7@users.noreply.github.com>
2025-02-07 00:23:42 +0300

    Update firebase.ts

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCAD2khLqzodNIBCWhYTlomZofQn7qu6iQ",
-  authDomain: "instreet2.firebaseapp.com",
-  projectId: "instreet2",
-  storageBucket: "instreet2.firebasestorage.app",
-  messagingSenderId: "1016870610831",
-  appId: "1:1016870610831:web:013d8761f2538a638bcdfa",
-  measurementId: "G-VVYJKD5V5Q",
+  apiKey: "AIzaSyCB7Q-h3I1sa5ixuvAxsyMGeuPe5HdoY4Y",
+  authDomain: "instreet3-c5552.firebaseapp.com",
+  projectId: "instreet3-c5552",
+  storageBucket: "instreet3-c5552.firebasestorage.app",
+  messagingSenderId: "438802023879",
+  appId: "1:438802023879:web:f14862bfea47d9962b45e2",
+  measurementId: "G-EJMRT8YD2M"
 };
 
 // Initialize Firebase
@@ -26,11 +26,11 @@ export const auth = getAuth(firebaseApp);
 
 // Initialize Analytics (browser only)
 if (typeof window !== "undefined") {
-  try {
-    getAnalytics(firebaseApp);
-  } catch (error) {
-    console.warn("Analytics could not be initialized:", error);
-  }
+    try {
+        getAnalytics(firebaseApp);
+    } catch (error) {
+        console.warn("Analytics could not be initialized:", error);
+    }
 }
 
 export { firebaseApp };
